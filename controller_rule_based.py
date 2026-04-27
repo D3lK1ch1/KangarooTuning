1d4f5532f30bb3f44e98e30a19407f9f8e26ec2b
D3lK1ch1 <dhizkch@gmail.com>
2026-04-27 14:56:46 +1000

    Fix controller, with removing dead class, key-presence bug, hardcoded position, spinning beam

--- controller_rule_based.py
+++ controller_rule_based.py
@@ -26,9 +26,10 @@ class RuleBasedController:
             action["adjust_mid_freq_intensity"] = 0.8
             
         elif distance_to_corridor > 10:
+            herd_center = state.get("herd_center", (5, 25))
             angle_to_corridor = np.arctan2(
-                (config.CORRIDOR_Y_MIN + config.CORRIDOR_Y_MAX) / 2 - 25,
-                config.CORRIDOR_X - 5
+                (config.CORRIDOR_Y_MIN + config.CORRIDOR_Y_MAX) / 2 - herd_center[1],
+                config.CORRIDOR_X - herd_center[0]
             )
             action["adjust_beam_direction"] = angle_to_corridor
             action["adjust_mid_freq_intensity"] = 0.8
@@ -43,23 +44,22 @@ class RuleBasedController:
         return action
     
     def apply_action(self, action, acoustic_field: AcousticField):
-        if "adjust_beam_direction" in action:
-            current_angle = acoustic_field.beam_angle
-            new_angle = current_angle + action["adjust_beam_direction"]
+        if action["adjust_beam_direction"] != 0.0:
+            new_angle = action["adjust_beam_direction"]
             acoustic_field.set_mid_freq_beam(new_angle, acoustic_field.beam_intensity)
-            
-        if "adjust_mid_freq_intensity" in action:
+
+        if action["adjust_mid_freq_intensity"] != 0.0:
             intensity = action["adjust_mid_freq_intensity"]
             acoustic_field.set_mid_freq_beam(
-                acoustic_field.beam_angle, 
+                acoustic_field.beam_angle,
                 intensity,
                 acoustic_field.beam_origin
             )
-            
-        if "activate_cue" in action:
+
+        if action["activate_cue"]:
             acoustic_field.set_social_cue(action["activate_cue"], 0.3)
-            
-        if "increase_ultrasound" in action:
+
+        if action["increase_ultrasound"]:
             acoustic_field.set_ultrasound(action["increase_ultrasound"], 0.9)
             
         if "decrease_all" in action and action["decrease_all"]:
@@ -70,30 +70,4 @@ class RuleBasedController:
             acoustic_field.set_social_cue(False, 0)
             acoustic_field.set_ultrasound(False, 0)
         
-        acoustic_field.update_fields()
-
-
-class RuleBasedControllerSimple:
-    def __init__(self):
-        self.name = "RuleBasedSimple"
-        
-    def get_action(self, state):
-        distance_to_corridor = state["distance_to_corridor"]
-        distance_to_road = state["distance_to_road"]
-        
-        if distance_to_road < 5:
-            return "increase_ultrasound"
-        elif distance_to_corridor > 10:
-            return "point_to_corridor"
-        else:
-            return "maintain"
-    
-    def apply_action(self, action, acoustic_field: AcousticField):
-        if action == "increase_ultrasound":
-            acoustic_field.set_ultrasound(True, 0.9)
-            
-        elif action == "point_to_corridor":
-            angle = np.arctan2(10, 40)
-            acoustic_field.set_mid_freq_beam(angle, 0.8)
-            
         acoustic_field.update_fields()
